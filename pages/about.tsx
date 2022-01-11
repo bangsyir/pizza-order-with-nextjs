28101c212f4996d9cfbc4e51f96b2a4932050491
bangsyir <bangsyir@gmail.com>
2022-01-11 23:49:14 +0800

    create: about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../components/Navbar";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <main className="px-12 text-center">
+        <h1 className="text-3xl font-bold">
+          This is about page
+        </h1>
+      </main>
+    </div>
+  )
+};
