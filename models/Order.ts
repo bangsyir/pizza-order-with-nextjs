eb85cafadcfdf98b1e78b070d377181d5054aa62
bangsyir <bangsyir@gmail.com>
2022-01-30 16:20:15 +0800

    create: mongoose Model

---
+++ models/Order.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const OrderSchema = new mongoose.Schema({
+  customer: {
+    type: String,
+    required:true,
+    maxlength:60
+  },
+  address: {
+    type: String,
+    required:true,
+    maxlength:200
+  },
+  phone: {
+    type:String,
+    required: true,
+    maxlenght:15
+  },
+  total: {
+    type: Number,
+    required:true
+  },
+  status: {
+    type:Number,
+    default: 0
+  },
+  method: {
+    type:Number,
+    required: true
+  },
+},{timestamps: true})
+
+export default mongoose.models.Order || mongoose.model("Order", OrderSchema)
