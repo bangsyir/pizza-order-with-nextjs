54f3d16bfc1c385c6ca53641bd2121e035cd7c09
bangsyir <bangsyir@gmail.com>
2022-01-30 16:21:05 +0800

    update: view

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,22 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
-type DataType = {
-  item: {
-    id: number
-    title: string
-    description: string
-  }
-}
-export const ItemCard = (props: DataType) => {
+export const ItemCard = ({item}:any) => {
   return (
-    <div className="bg-white p-2 rounded-md  border border-gray-400">
+    <div className="bg-white p-2 rounded-md">
       <div className='flex justify-center'>
           <Image src="/img/pizza.png" width={200} height={200} />
       </div>
-      <p className='font-bold'>{props.item.title}</p>
-      <p className='font-light'>{props.item.description}</p>
-      <button className='p-2 bg-orange-700 text-orange-200 rounded-lg font-bold mt-2'>Add To cart</button>
+      <p className='font-bold uppercase text-lg text-orange-700'>{item.title}</p>
+      <p className='font-bold uppercase text-lg text-dark-700'>${item.prices[0]}.00</p>
+      <p className='font-light truncate'>{item.desc}</p>
+      <Link href={`/foods/${item._id}`}>
+        <button className='p-1 bg-orange-500 text-orange-200 rounded-lg font-bold mt-2 mx-2'>View Item</button>
+      </Link>
     </div>
   )
 };
-
